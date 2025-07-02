fd8edbfb75bdc30d0c1884dd4cec08a98da4e788
sonny <sonny@google.com>
2025-07-02 10:19:27 +0900

    [refactoring] p58

--- prime-factors.cpp
+++ prime-factors.cpp
@@ -7,22 +7,23 @@ public:
 		vector<int> result = { };
 
 		if (number > 1) {
+			int divisor = 2;
 			if (number == 4) {
-				while (number % 2 == 0) {
-					result.push_back(2);
-					number /= 2;
+				while (number % divisor == 0) {
+					result.push_back(divisor);
+					number /= divisor;
 				}
 			}
 			else if (number == 6) {
-				while (number % 2 == 0) {
-					result.push_back(2);
-					number /= 2;
+				while (number % divisor == 0) {
+					result.push_back(divisor);
+					number /= divisor;
 				}
 				while (number % 3 == 0) {
 					result.push_back(3);
 					number /= 3;
 				}
-				
+					
 			}
 			else
 				result.push_back(number);
